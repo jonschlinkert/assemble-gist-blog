9da83eca34ab39b128a49e7e4b3a5f75ce63b1c5
Jon Schlinkert <dev@sellside.com>
2013-07-16 16:40:02 -0400

    several updates to project structure, organization. Also corrected metadata in package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 /*
  * assemble-gist-blog
  * https://github.com/assemble/assemble-gist-blog
- * Copyright (c) 2013 
+ * Copyright (c) 2013
  * Licensed under the MIT license.
  */
 
@@ -13,21 +13,33 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     assemble: {
-      pages: {
-        options: {
-          flatten: true,
-          assets: 'assets',
-          layout: 'src/templates/layouts/default.hbs',
-          data: 'src/data/*.{json,yml}',
-          partials: 'src/templates/partials/*.hbs'
-        },
+      options: {
+        flatten: true,
+        assets: 'assets',
+        layout: 'src/layouts/default.hbs',
+        data: 'src/data/*.{json,yml}'
+      },
+      blog: {
         files: {
-          './': ['src/templates/pages/*.hbs']
+          './': ['src/*.hbs']
         }
       }
     },
 
-    // Before generating any new files, 
+    less: {
+      options: {
+        paths:   'vendor/bootstrap/less',
+        imports: {
+          less: ['mixins.less', 'variables.less']
+        }
+      },
+      bootstrap: {
+        src:  ['vendor/bootstrap/less/bootstrap.less', 'src/gist-overrides.less'],
+        dest: 'assets/bootstrap.css'
+      }
+    },
+
+    // Before generating any new files,
     // remove any previously-created files.
     clean: {
       example: ['*.html']
@@ -36,6 +48,7 @@ module.exports = function(grunt) {
 
   // Load npm plugins to provide necessary tasks.
   grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('assemble-less');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default tasks to be run.
--- README.md
+++ README.md
@@ -4,12 +4,53 @@
 
 It's hard to believe when you see how fast the pages render, but all of the content in the [example pages](http://assemble.github.io/assemble-gist-blog) is from Gists.
 
-## Also see 
 
+## Getting Started
+
+Download this project:
+
+```bash
+git clone https://github.com/assemble/boilerplate-gist-blog.git
+```
+
+Install the necessary dependencies to run the build:
+
+```bash
+npm i
+```
+Once dependencies are installed, you may build the project by running `grunt` or `grunt assemble`.
+
+### Styles
+
+Currently the `assemble.css` stylesheet is used in the examples, but the project is already setup to use Bootstrap if you want to edit the styles.
+
+To do so, from the root of the project `git clone` Bootstrap:
+
+```bash
+git clone -b 3.0.0-wip https://github.com/twitter/bootstrap.git "vendor/bootstrap"
+```
+(the "less" task is currently configured to use the `vendor` directory)
+
+In `./src/layouts/default.hbs`, change this:
+
+```html
+<link rel="stylesheet" href="{{assets}}/assemble.css">
+```
+to this:
+
+```html
+<link rel="stylesheet" href="{{assets}}/bootstrap.css">
+```
+
+Then run `grunt less` to compile.
+
+
+## Related Projects
+
+### [Assemble's documentation](https://assemble.io) →
 ### [assemble-boilerplates](https://github.com/assemble/assemble-boilerplates) →
 ### [grunt-init-assemble](https://github.com/assemble/grunt-init-assemble-helper) →
 ### [grunt-init-assemble-helper](https://github.com/assemble/grunt-init-assemble-helper) →
-
 
 
 ## Contributing
